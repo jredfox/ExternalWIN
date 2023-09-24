53b3c326952e728476eb4ca8588be4bd2748098e
jredfox <jredfox_@outlook.com>
2023-09-24 14:23:11 -0500

    Update IMG2WIM.bat

--- src/IMG2WIM.bat
+++ src/IMG2WIM.bat
@@ -1,3 +1,3 @@
 @Echo off
 set /p img=Enter Image(VHD, VHDX, ISO, ESD):
-powershell -ExecutionPolicy Bypass -File "%dp0~IMG2WIM.ps1"
+powershell -ExecutionPolicy Bypass -File "%~dp0IMG2WIM.ps1" -Image "%img%"
