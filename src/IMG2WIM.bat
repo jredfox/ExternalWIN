43b2161ae572acca962259f874fd6afcc47ed57a
jredfox <jredfox_@outlook.com>
2023-09-25 03:25:32 -0500

    Update IMG2WIM.bat
    fix spaced files

--- src/IMG2WIM.bat
+++ src/IMG2WIM.bat
@@ -1,3 +1,4 @@
 @Echo off
 set /p img=Enter Image(VHD, VHDX, ISO, ESD):
-powershell -ExecutionPolicy Bypass -File "%~dp0IMG2WIM.ps1" -Image "%img%"
+powershell -ExecutionPolicy Bypass -File "%~dp0IMG2WIM.ps1" -Image %img%
+pause
