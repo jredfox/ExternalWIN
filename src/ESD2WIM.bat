3a03fb7fbb8eaf72cfc3f107a68d32646e033158
jredfox <jredfox_@outlook.com>
2023-09-25 09:38:03 -0500

    ESD2WIM
    added ESD2WIM support

---
+++ src/ESD2WIM.bat
@@ -0,0 +1,22 @@
+@Echo off
+setlocal ENABLEDELAYEDEXPANSION
+IF "%~1%" EQU "" (
+set /p esd=Enter ESD File:
+set sp=true
+) else (
+set sp=false
+set esd=%~1%
+)
+set esd=%esd:"=%
+set wim=%esd:.esd=.wim%
+for /L %%A in (1, 1, 256) Do (
+echo Extracting Index %%A
+dism /Export-Image /SourceImageFile:"%esd%" /SourceIndex:%%A /DestinationImageFile:"%wim%"
+IF !ERRORLEVEL! NEQ 0 (GOTO END)
+echo[
+echo Finished Extracting Index %%A
+echo[
+)
+:END
+IF %sp% EQU true
+pause
--- src/IMG2WIM.ps1
+++ src/IMG2WIM.ps1
@@ -3,6 +3,11 @@ param(
 [string]$Image
 )
 $Directory = (Get-Item -Path $Image).Directory
+$extn = [IO.Path]::GetExtension($Image)
+if($extn -eq ".WIM")
+{
+  Write-Host "Error: Cannot Convert WIM $Image TO WIM As it's Already a WIM"
+}
 #start cleanup
 $wimfile = "$Directory\" + (Get-Item -Path $Image).BaseName + ".wim"
 if (test-path $wimfile) {
@@ -11,15 +16,20 @@ Remove-Item $wimfile
 DisMount-DiskImage -ImagePath $Image | Out-Null
 #end cleanup
 #support ISO and other possible virtual disk images
-$extn = [IO.Path]::GetExtension($Image)
 if ($extn -ne ".VHD" -and $extn -ne ".VHDX")
 {
-   Write-Host here
-   $name = (Get-Item -Path $Image).BaseName
-   $MountedVhd = Mount-DiskImage -ImagePath $Image -PassThru
-   $drive = (Get-DiskImage -ImagePath $Image | Get-Volume).DriveLetter + ":"
-   dism /Capture-Image /ImageFile:"$wimfile" /CaptureDir:"$drive" /Name:"$name"
-   DisMount-DiskImage -ImagePath $Image | Out-Null
+   if($extn -ne ".ESD")
+   {
+        $name = (Get-Item -Path $Image).BaseName
+         $MountedVhd = Mount-DiskImage -ImagePath $Image -PassThru
+         $drive = (Get-DiskImage -ImagePath $Image | Get-Volume).DriveLetter + ":"
+         dism /Capture-Image /ImageFile:"$wimfile" /CaptureDir:"$drive" /Name:"$name"
+         DisMount-DiskImage -ImagePath $Image | Out-Null
+   }
+   else 
+   {
+      cmd.exe /c "$PSScriptRoot\ESD2WIM.bat" "$Image"
+   }
    exit
 }
 $MountPath = "C:\Temp\VHD\" # This folder must pre-exist
