33ddb5df68d8fcca6c52f75fe2eaa029f5de1a5f
jredfox <jredfox_@outlook.com>
2023-10-06 12:06:03 -0500

    fix

--- src/FileExplorerPopUp-Disable.bat
+++ src/FileExplorerPopUp-Disable.bat
@@ -1,5 +1,5 @@
 @Echo OFF
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\Explorer" /v NoAutoplayfornonVolume /t REG_DWORD /d 1 /f
-reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v NoAutorun /t REG_DWORD /d 1 /f
-reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\policies\Explorer" /v NoDriveTypeAutoRun /t REG_DWORD /d 255 /f
+REM reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v NoAutorun /t REG_DWORD /d 1 /f
+reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v NoDriveTypeAutoRun /t REG_DWORD /d 255 /f
 echo File Explorer Popups Disabled
