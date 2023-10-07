eec34405e87d12d1e93900473da7e89a3936b021
jredfox <jredfox_@outlook.com>
2023-10-06 21:54:08 -0500

    disable autoplay
    disabling autoplay seems to behave better for USB drives even though it shouldn't effect it it does. There still is no work around for creating an MSR partition without the format popup.

--- src/FileExplorerPopUp-Enable.bat
+++ src/FileExplorerPopUp-Enable.bat
@@ -1,6 +1,6 @@
 @Echo OFF
 reg delete "HKLM\SOFTWARE\Policies\Microsoft\Windows\Explorer" /v NoAutoplayfornonVolume /f
-REM reg delete "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v NoAutorun /f
+reg delete "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v NoAutorun /f
 reg delete "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v NoDriveTypeAutoRun /f
 reg delete "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v NoAutoplayfornonVolume /f
 echo File Explorer Popups Enabled
