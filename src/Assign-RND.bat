9f35ada011f9f47a7d720d0249e3ad32ebe6730a
jredfox <jredfox_@outlook.com>
2023-10-07 20:31:08 -0500

    fix #2

--- src/Assign-RND.bat
+++ src/Assign-RND.bat
@@ -4,5 +4,5 @@ set let=0
 set "drives=DEFGHIJKLMNOPQRSTUVWXYZABC"
 for /f "delims=:" %%A in ('wmic logicaldisk get caption') do set "drives=!drives:%%A=!"
 set let=%drives:~0,1%
-echo Assign Letter^:%let% in Partition %par% of Disk %disk%
+echo Assign Drive Letter^: %let% for Partition %par% of Disk %disk%
 diskpart /s "%~dp0Assign.txt"
