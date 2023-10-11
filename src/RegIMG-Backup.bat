9687374bea5c7c0dce40ba799afb485aa006a589
jredfox <jredfox_@outlook.com>
2023-10-10 19:30:41 -0500

    call PP more often

--- src/RegIMG-Backup.bat
+++ src/RegIMG-Backup.bat
@@ -1,6 +1,7 @@
 @Echo Off
 setlocal enableDelayedExpansion
 call :checkAdmin "You Need to run ExternalWIN Scripts as Administrator in order to use them"
+call :PP
 
 set hivelist=%~dp0hivelist.txt
 IF NOT EXIST "%hivelist%" (
@@ -67,5 +68,19 @@ IF %ERRORLEVEL% NEQ 0 (
 echo %~1
 pause
 exit 1
+)
+exit /b
+
+:PP
+REM ######## WinPE support change the power plan to maximize perforamnce #########
+set winpe=F
+REM Check if we are in WINPE. If Either where or powershell is missing and X:\ Exists we are in WinPE
+IF NOT EXIST "X:\" (exit /b)
+where powershell >nul 2>&1
+IF !ERRORLEVEL! NEQ 0 (
+winpe=T
+FOR /f "delims=" %%a in ('POWERCFG -GETACTIVESCHEME') DO @SET powerplan="%%a"
+powercfg /s 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c
+echo changed powerplan of !powerplan! to high performance 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c
 )
 exit /b
