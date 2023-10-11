049f252e5508bd7b007e020dd5dd5af899ca080f
jredfox <jredfox_@outlook.com>
2023-10-11 00:29:21 -0500

    derp

--- src/RegIMG-Apply.bat
+++ src/RegIMG-Apply.bat
@@ -33,11 +33,11 @@ exit /b
 :PP
 REM ######## WinPE support change the power plan to maximize perforamnce #########
 set winpe=F
-REM Check if we are in WINPE. If Either where or powershell is missing and X:\ Exists we are in WinPE
+REM Check if we are in WINPE. If Either where or powershell is missing and X Drive Exists we are in WinPE
 IF NOT EXIST "X:\" (exit /b)
 where powershell >nul 2>&1
 IF !ERRORLEVEL! NEQ 0 (
-winpe=T
+set winpe=T
 FOR /f "delims=" %%a in ('POWERCFG -GETACTIVESCHEME') DO @SET powerplan="%%a"
 powercfg /s 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c
 echo changed powerplan of !powerplan! to high performance 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c
