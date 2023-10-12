aba70db8e61e326e068eb588324e0acca954e168
jredfox <jredfox_@outlook.com>
2023-10-12 02:37:42 -0500

    refactor p1

--- src/Assign-RND.bat
+++
@@ -1,8 +0,0 @@
-@Echo Off
-setlocal enableDelayedExpansion
-set let=0
-set "drives=DEFGHIJKLMNOPQRSTUVWXYZABC"
-for /f "delims=:" %%A in ('wmic logicaldisk get caption') do set "drives=!drives:%%A=!"
-set let=%drives:~0,1%
-echo Assign Drive Letter^: %let% for Partition %par% of Disk %disk%
-diskpart /s "%~dp0Assign.txt"
--- src/REG-Backup.bat
+++
@@ -1,7 +0,0 @@
-@ECHO OFF
-reg export HKLM "%~dp0HKLM.reg"
-reg export HKCU "%~dp0HKCU.reg"
-reg export HKCR "%~dp0HKCR.reg"
-reg export HKU "%~dp0HKU.reg"
-reg export HKCC "%~dp0HKCC.reg"
-pause
