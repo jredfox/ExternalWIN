9dab6590441d1c1532427da607b0c636ad5ee34e
jredfox <jredfox_@outlook.com>
2023-10-14 08:17:20 -0700

    WIP

--- ExternalWIN/WIM-Capture.bat
+++ ExternalWIN/WIM-Capture.bat
@@ -2,27 +2,71 @@
 setlocal enableDelayedExpansion
 call :checkAdmin "You Need to run ExternalWIN Scripts as Administrator in order to use them"
 call :PP
-set /p wim="Enter File Name:"
-set wim=%wim:"=%
-set /p desc="Enter Description:"
+
+:SELFILE
+set msg="Enter File Name:"
+IF "%winpe%" EQU "T" (set msg=Enter File Save As^:)
+set /p wim=%msg%
+set wim=!wim:"=!
+IF "!winpe!" EQU "T" (
+IF "!wim:~1,1!" NEQ ":" (
+echo Enter Full Path of The File to Save As
+GOTO SELFILE
+)
+)
+set /p desc="Enter Name of Backup:"
 diskpart /s "%~dp0ld.txt"
 set /p disk="Input Disk Number:"
 diskpart /s "%~dp0dd.txt"
-set /p let="Enter Drive:"
-set let=%let:"=%
-md %userprofile%\Documents\%ComputerName%\WIMS
-set wim=%userprofile%\Documents\%ComputerName%\WIMS\%wim%.wim
-set wim=%wim:.wim.wim=.wim%
-dism /capture-image /imagefile:"%wim%" /capturedir:"%let%" /name:"%ComputerName%" /Description:"%desc%" /compress:maximum
-IF ERRORLEVEL 1 echo ######################### & echo Try Changing "%let%" to "%let:~0,1%:" or "%let:~0,1%" to capture the entire drive. This is a DISM.exe bug not an issue with the script & echo#########################
-echo "SAVED WIM TO: %wim%"
+set /p let="Enter Capture Drive:"
+set let=!let:"=!
+REM IF We are only capturing the whole drive fix the drive letter to make DISM happy
+IF "!let:~3,3!" EQU "" (set let=!let:~0,1!^:)
+
+REM ######## Find the ComputerName ############
+set drive=!let:~0,1!
+set comp=!drive!:\Windows\System32\Config\SYSTEM
+IF NOT EXIST "!comp!" (
+set /p drive="Enter Windows Drive Letter:"
+set drive=!drive:"=!
+set drive=!drive:~0,1!
+set comp=!drive!:\Windows\System32\Config\SYSTEM
+)
+IF NOT EXIST "!comp!" (
+set /p COMPNAME="AutoDetection Failed Enter Computer Name:"
+set COMPNAME=!COMPNAME:"=!
+GOTO INSTALL
+)
+reg load HKLM\OfflineSystem "!comp!" >nul 2>&1
+IF !ERRORLEVEL! NEQ 0 (GOTO CHK)
+for /f "tokens=3*" %%a in ('reg query HKLM\OfflineSystem\ControlSet001\Control\ComputerName\ComputerName /v ComputerName') do (set COMPNAME=%%a)
+reg unload HKLM\OfflineSystem
+
+:CHK
+REM ### IF Computer Name is Still Blank It Means We are Capturing on the Same Computer As We are Currently Running On ###
+IF "!COMPNAME!" EQU "" (set COMPNAME=!ComputerName!)
+
+:INSTALL
+echo Capturing !let! on Computer !COMPNAME!
+REM If USER Specified a Relitive Path Then Store it in Documents
+IF "!wim:~1,1!" NEQ ":" (set wim=!USERPROFILE!\Documents\!COMPNAME!\WIMS\!wim!)
+set wim=!wim!.wim
+set wim=!wim:.wim.wim=.wim!
+md "%wim%" >nul 2>&1
+rd /q "%wim%" >nul 2>&1
+
+IF NOT EXIST "%wim%" (
+dism /capture-image /imagefile:"%wim%" /capturedir:"%let%" /name:"%desc%" /Description:"%COMPNAME% On %date% %time%" /compress:maximum
+) ELSE (
+dism /append-image /imagefile:"%wim%" /capturedir:"%let%" /name:"%desc%" /Description:"%COMPNAME% On %date% %time%"
+)
 pause
 exit /b
 
 :checkAdmin
 net session >nul 2>&1
-IF %ERRORLEVEL% NEQ 0 (
-echo %~1
+IF !ERRORLEVEL! NEQ 0 (
+echo !~1
 pause
 exit 1
 )
