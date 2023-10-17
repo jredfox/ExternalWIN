b5e255a2b6802738ed6ed5192e9dd2fa55fc3f4e
jredfox <jredfox_@outlook.com>
2023-10-17 06:56:58 -0500

    1.0.4

--- ExternalWIN/WinInstall.bat
+++ ExternalWIN/WinInstall.bat
@@ -1,7 +1,7 @@
 @Echo Off
 setlocal enableDelayedExpansion
 call :checkAdmin "You Need to run ExternalWIN Scripts as Administrator in order to use them"
-title ExternalWin Version 1.0.3
+title ExternalWin Version 1.0.4
 call :PP
 call "%~dp0FileExplorerPopUp-Enable.bat" >nul 2>&1
 Reagentc /enable >nul 2>&1
