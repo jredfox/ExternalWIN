bede6eb63852fb0274008e58dc5532e17871b2c6
jredfox <jredfox_@outlook.com>
2023-10-16 20:44:18 -0500

    fix pp

--- ExternalWIN/WinAssignRecovery.bat
+++ ExternalWIN/WinAssignRecovery.bat
@@ -1,6 +1,7 @@
 @Echo Off
 setlocal enableDelayedExpansion
 call :checkAdmin "You Need to run ExternalWIN Scripts as Administrator in order to use them"
+call :PP
 call "%~dp0FileExplorerPopUp-Enable.bat" >nul 2>&1
 Reagentc /enable >nul 2>&1
 diskpart /s "%~dp0ld.txt"
@@ -47,5 +48,19 @@ IF %ERRORLEVEL% NEQ 0 (
 echo %~1
 pause
 exit 1
+)
+exit /b
+
+:PP
+REM ######## WinPE support change the power plan to maximize perforamnce #########
+set winpe=F
+REM Check if we are in WINPE. If Either where or powershell is missing and X Drive Exists we are in WinPE
+IF NOT EXIST "X:\" (exit /b)
+where powershell >nul 2>&1
+IF !ERRORLEVEL! NEQ 0 (
+set winpe=T
+FOR /f "delims=" %%a in ('POWERCFG -GETACTIVESCHEME') DO @SET powerplan="%%a"
+powercfg /s 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c
+echo changed powerplan of !powerplan! to high performance 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c
 )
 exit /b
