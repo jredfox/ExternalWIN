518d8a66a42e7d69b36be2b8c724985f6048399f
jredfox <jredfox_@outlook.com>
2023-10-22 15:49:09 -0500

    1.0.7

--- ExternalWIN/WinInstallVHDX.bat
+++ ExternalWIN/WinInstallVHDX.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
 Setlocal EnableDelayedExpansion
 call :checkAdmin "You Need to run ExternalWIN Scripts as Administrator in order to use them"
-title ExternalWin Version 1.0.6 VHDX
+title ExternalWin Version 1.0.7 VHDX
 call :PP
 
 rem ############ CLEANUP ##################
