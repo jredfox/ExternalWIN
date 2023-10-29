276e73d59a26613eb9a261495aaf172c9df53d9c
jredfox <jredfox_@outlook.com>
2023-10-29 10:44:59 -0500

    1.0.10 fix BSOD

--- ExternalWIN/WinInstallVHDX-Custom.bat
+++ ExternalWIN/WinInstallVHDX-Custom.bat
@@ -2,7 +2,7 @@
 setlocal enableDelayedExpansion
 call :checkAdmin "You Need to run ExternalWIN Scripts as Administrator in order to use them"
 IF !ERRORLEVEL! NEQ 0 (exit /b !ERRORLEVEL!)
-title ExternalWin Version 1.0.9 VHDX
+title ExternalWin Version 1.0.10 VHDX
 call "%~dp0FileExplorerPopUp-Enable.bat" >nul 2>&1
 set /p vdisk="Input VHDX File:"
 set vdisk=%vdisk:"=%
