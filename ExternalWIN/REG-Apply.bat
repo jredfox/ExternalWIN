4c4547b4cb59ef1c0a133c4428edac8cd7ded4e0
jredfox <jredfox_@outlook.com>
2023-10-30 05:24:37 -0500

    improve REG-Apply.bat

--- ExternalWIN/REG-Apply.bat
+++ ExternalWIN/REG-Apply.bat
@@ -1,14 +1,21 @@
 @Echo off
 setlocal enableDelayedExpansion
-reg import "%~dp0HKLM.reg"
-reg import "%~dp0HKCU.reg"
-reg import "%~dp0HKCR.reg"
-reg import "%~dp0HKU.reg"
-reg import "%~dp0HKCC.reg"
-regedit /s "%~dp0HKLM.reg"
-regedit /s "%~dp0HKCU.reg"
-regedit /s "%~dp0HKCR.reg"
-regedit /s "%~dp0HKU.reg"
-regedit /s "%~dp0HKCC.reg"
+set regdir=%HOMEDRIVE%\ExternalWIN\Backups\REG
+FOR /D %%D in ("%regdir%\*") DO (echo %%D)
+:SEL
+set /p regbackup="Enter Reg Backup Dir:"
+set regbackup=!regbackup:"=!
+IF NOT EXIST "!regbackup!" (set regbackup=!regdir!\!regbackup!)
+IF NOT EXIST "!regbackup!" (GOTO SEL)
+reg import "!regbackup!\HKLM.reg"
+reg import "!regbackup!\HKCU.reg"
+reg import "!regbackup!\HKCR.reg"
+reg import "!regbackup!\HKU.reg"
+reg import "!regbackup!\HKCC.reg"
+regedit /s "!regbackup!\HKLM.reg"
+regedit /s "!regbackup!\HKCU.reg"
+regedit /s "!regbackup!\HKCR.reg"
+regedit /s "!regbackup!\HKU.reg"
+regedit /s "!regbackup!\HKCC.reg"
 echo Finished Importing REG Backups
 pause
