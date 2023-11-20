b289d337211d81c194fb0bcf5a701a0c72d60262
jredfox <jredfox_@outlook.com>
2023-11-20 15:04:26 -0600

    PrintActual OneDrive files
    fixed to print actual OneDrive files. needs to skip files that are already in blacklisted directories

--- ExternalWIN/OneDriveLinks.bat
+++ ExternalWIN/OneDriveLinks.bat
@@ -3,7 +3,8 @@ setlocal ENABLEDELAYEDEXPANSION
 set drive=%~1
 set drive=!drive:~0,1!
 echo Indexing The !drive! Drive
-set extindex=%TMP%\EXTWINDirIndex.txt
-dir /S /A^:D-L /B !drive!^:\ >!extindex!
-call "%~dp0OneDriveLinkFinder.exe" "!extindex!"
-pause
+FOR /F "delims=" %%I in ('dir /S /B /A^:LO !drive!^:\') DO (
+set path=%%I
+set path=!path:~2!
+echo !path!
+)
