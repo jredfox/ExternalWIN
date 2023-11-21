8c56dba40662db3c3b720356c85b404fd413006a
jredfox <jredfox_@outlook.com>
2023-11-21 08:27:21 -0600

    Fix WinPE 10
    fix WinPE for Windows 10 Len.vbs not working

--- ExternalWIN/PrintOneDriveLinks.bat
+++ ExternalWIN/PrintOneDriveLinks.bat
@@ -8,7 +8,7 @@ del /F "!dirs!" /s /q /a >nul 2>&1
 del /F "!EXTIndex!" /s /q /a >nul 2>&1
 call "%~dp0PrintOneDrive.bat" "!drive!" >!dirs!
 echo Indexing The !drive! Drive
-dir /S /B /A:LO !drive!^:\ >!EXTIndex!
+dir /S /B /A^:LO !drive!^:\ >!EXTIndex!
 FOR /F "usebackq delims=" %%I IN ("!EXTIndex!") DO (
 set path=%%I
 set path=!path:~2!
@@ -38,5 +38,6 @@ IF /I "!Child:~0,%strlen%!" EQU "%Parent%" (set ISCHILD=T)
 exit /b
 
 :STRLEN
-FOR /F %%a IN ('cscript "%~dp0Len.vbs" "%~1"') DO (set strlen=%%a)
+set cmd=cscript "%~dp0Len.vbs" "%~1"
+FOR /F %%a IN ('!cmd!') DO (set strlen=%%a)
 exit /b
