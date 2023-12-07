aba5203c97090e81809d7fb4b0a8237e15a9a893
jredfox <jredfox_@outlook.com>
2023-12-07 15:22:50 -0600

    call PP
    call PP more often

--- ExternalWIN/ESD2WIM.bat
+++ ExternalWIN/ESD2WIM.bat
@@ -2,6 +2,7 @@
 setlocal ENABLEDELAYEDEXPANSION
 call :checkAdmin "You Need to run ExternalWIN Scripts as Administrator in order to use them"
 IF !ERRORLEVEL! NEQ 0 (exit /b !ERRORLEVEL!)
+call :PP
 set comp=maximum
 IF "%~1" EQU "" (
 set /p esd="Enter ESD File:"
@@ -50,5 +51,19 @@ exit /b
 ) ELSE (
 set /A WIMSIZE=!WIMSIZE! + 1
 GOTO LOOPINDEX
+)
+exit /b
+
+:PP
+REM ######## WinPE support change the power plan to maximize perforamnce #########
+set winpe=F
+REM Check if we are in WINPE. If Either where or powershell is missing and X Drive Exists we are in WinPE
+IF NOT EXIST "X:\" (exit /b)
+where powershell >nul 2>&1
+IF !ERRORLEVEL! NEQ 0 (
+set winpe=T
+FOR /f "delims=" %%a in ('POWERCFG -GETACTIVESCHEME') DO @SET powerplan="%%a"
+powercfg /s 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c
+echo changed powerplan of !powerplan! to high performance 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c
 )
 exit /b
--- ExternalWIN/Wim-Delete-Index.bat
+++ ExternalWIN/Wim-Delete-Index.bat
@@ -2,6 +2,7 @@
 setlocal ENABLEDELAYEDEXPANSION
 call :checkAdmin "You Need to run ExternalWIN Scripts as Administrator in order to use them"
 IF !ERRORLEVEL! NEQ 0 (exit /b !ERRORLEVEL!)
+call :PP
 set /p wim="Enter WIM/ESD:"
 set wim=%wim:"=%
 dism /get-imageinfo /imagefile:"%wim%"
@@ -18,5 +19,19 @@ IF %ERRORLEVEL% NEQ 0 (
 echo %~1
 pause
 exit /b 1
+)
+exit /b
+
+:PP
+REM ######## WinPE support change the power plan to maximize perforamnce #########
+set winpe=F
+REM Check if we are in WINPE. If Either where or powershell is missing and X Drive Exists we are in WinPE
+IF NOT EXIST "X:\" (exit /b)
+where powershell >nul 2>&1
+IF !ERRORLEVEL! NEQ 0 (
+set winpe=T
+FOR /f "delims=" %%a in ('POWERCFG -GETACTIVESCHEME') DO @SET powerplan="%%a"
+powercfg /s 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c
+echo changed powerplan of !powerplan! to high performance 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c
 )
 exit /b
