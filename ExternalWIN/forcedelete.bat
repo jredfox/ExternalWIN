e60b48021e7762e661b0adc401fab8b278582cb4
jredfox <jredfox_@outlook.com>
2023-12-31 15:43:30 -0600

    fix forcedelete.bat
    fixed forcedelete.bat from accidental deletion from relative paths accidental deletion of OS files from SYMLINKD and JUNCTION Dirs

--- ExternalWIN/forcedelete.bat
+++ ExternalWIN/forcedelete.bat
@@ -1,15 +1,26 @@
 @Echo off
 setlocal enableDelayedExpansion
 call :checkAdmin "You Need to run ExternalWIN Scripts as Administrator in order to use them"
+call :GETDIRSAFE
 IF !ERRORLEVEL! NEQ 0 (exit /b !ERRORLEVEL!)
+set dir=%~1
+REM Stop Accidental Deletion of System32 or current directory of Command Prompt
+IF "!dir:~1,1!" NEQ ":" (exit /b)
 echo WARNING^: Forcibly Deleting Files Is Dangerous If You Do Not Know What you are doing
 set /p warn="Do you Wish To Continue [Y/N]?"
 IF /I "!warn!" NEQ "Y" (exit /b)
-REM this is to force delete the extracted WIM files when mounting goes wrong. Do not Delete SYSTEM32 with this
-set dir=%~1
-IF "%~1" EQU "" (exit /b)
+REM Delete JUNCTION and SYMLIND Dirs to prevent accidental deletion of unintended folders
+FOR /F "delims=" %%I IN ('call "!direxe!" "!dir!" "TRUE" "B" "DL" 2^>nul') DO (
+echo RD^:%%I
+RD "%%I"
+)
 takeown /F "%dir%" /R /D Y
 icacls "%dir%" /T /C /grant administrators:F System:F everyone:F
+REM Delete JUNCTION and SYMLIND Dirs to prevent accidental deletion of unintended folders
+FOR /F "delims=" %%I IN ('call "!direxe!" "!dir!" "TRUE" "B" "DL" 2^>nul') DO (
+echo RD^:%%I
+RD "%%I"
+)
 del /F "%dir%" /s /q /a
 rmdir /s /q "%dir%"
 exit /b
@@ -21,3 +32,11 @@ echo %~1
 pause
 exit /b 1
 )
+exit /b
+
+:GETDIRSAFE
+set dirsafedir=%~dp0DirSafe
+set direxe=!dirsafedir!\DirSafe-x64.exe
+call "!direxe!" "/?" >nul 2>&1
+IF !ERRORLEVEL! NEQ 0 (set direxe=!dirsafedir!\DirSafe-x86.exe)
+exit /b
