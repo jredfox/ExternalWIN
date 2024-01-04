bf5dfad9af9c74e18a456d67f3a4281f107635b5
jredfox <jredfox_@outlook.com>
2024-01-04 15:29:39 -0600

    handle individual files
    handle individual files.

--- ExternalWIN/forcedelete.bat
+++ ExternalWIN/forcedelete.bat
@@ -12,6 +12,15 @@ echo WARNING^: Forcibly Deleting Files Is Dangerous If You Do Not Know What you 
 set /p warn="Do you Wish To Continue [Y/N]?"
 IF /I "!warn!" NEQ "Y" (exit /b)
 set glp=^\^\^?^\
+call :ISDIR "!dir!"
+REM ## Handle indivdual file deletion requests ##
+IF "!ISDIR!" EQU "F" (
+takeown /A /SKIPSL /F "!dir!" >nul 2>&1
+"!grantexe!" "!dir!"
+del /F "!glp!!dir!" /S /Q /A >nul 2>&1
+exit /b
+)
+REM ## Handle Entire Directories Recursively ##
 echo Takeown Dir^: "!dir!"
 takeown /A /SKIPSL /R /F "!dir!" /D Y >nul 2>&1
 "!grantexe!" "!dir!"
@@ -19,6 +28,25 @@ echo DEL Dir^: "!dir!"
 del /F "!glp!!dir!^\" /S /Q /A >nul 2>&1
 echo RMDIR Dir^: "!dir!"
 rmdir /S /Q "!glp!!dir!^\" >nul 2>&1
+exit /b
+
+:ISDIR
+set ATTR=%~a1
+call :CONTAINS "!ATTR!" "D"
+IF "!STRCONTAINS!" EQU "T" (set ISDIR=T) ELSE (set ISDIR=F)
+IF "!ISDIR!" EQU "T" (exit /b)
+call :CONTAINS "!ATTR!" "d"
+IF "!STRCONTAINS!" EQU "T" (set ISDIR=T) ELSE (set ISDIR=F)
+exit /b
+
+:CONTAINS
+REM contains function that doesn't support quotes in strings
+set str=%1
+set strs=%2
+set str=!str:"=!
+set strs=!strs:"=!
+set strnew=!str:%strs%=!
+IF "!str!" EQU "!strnew!" (set STRCONTAINS=F) ELSE (set STRCONTAINS=T)
 exit /b
 
 :checkAdmin
