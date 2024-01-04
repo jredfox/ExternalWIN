e79779af99a10912744e0137fc99d5c5ed17bcc8
jredfox <jredfox_@outlook.com>
2024-01-03 20:01:33 -0600

    Update PrintOneDriveLinks.bat

--- ExternalWIN/PrintOneDriveLinks.bat
+++ ExternalWIN/PrintOneDriveLinks.bat
@@ -11,9 +11,9 @@ REM Always Print anything found in the WDI folder as it should always be < 1GB t
 call "!direxe!" "!drive:~0,1!^:\Windows\System32\WDI" "TRUE" "B" "K" "0x9000601A;0x9000001A;0x9000101A;0x9000201A;0x9000301A;0x9000401A;0x9000501A;0x9000701A;0x9000801A;0x9000901A;0x9000A01A;0x9000B01A;0x9000C01A;0x9000D01A;0x9000E01A;0x9000F01A;0x80000021;0x0000F000" 2>nul
 REM IF the Directory Doesn't Exist do not Continue as the Dir command will freak out and take way too long
 IF NOT EXIST "!drive!" (exit /b)
-call "%~dp0PrintOneDrive.bat" "!drive!" >"!dirs!"
 REM Don't Scan The C Drive for OneDrive Links if there are no OneDrive Accounts found or If Disabled
 IF /I "!OneDriveLinkScan!" EQU "FALSE" (exit /b)
+call "%~dp0PrintOneDrive.bat" "!drive!" >"!dirs!"
 IF /I "!OptimizedWIMCapture!" EQU "TRUE" (
 call :ISBLANK "!dirs!"
 IF "!isBlank!" EQU "T" (exit /b)
