5ab9606617a1b19268ef4131c67fa3b35378bbb7
jredfox <jredfox_@outlook.com>
2024-01-06 18:59:59 -0600

    1.0.12

--- ExternalWIN/WinInstallVHDX-Custom.bat
+++ ExternalWIN/WinInstallVHDX-Custom.bat
@@ -2,7 +2,7 @@
 setlocal enableDelayedExpansion
 call :checkAdmin "You Need to run ExternalWIN Scripts as Administrator in order to use them"
 IF !ERRORLEVEL! NEQ 0 (exit /b !ERRORLEVEL!)
-title ExternalWin Version 1.0.11 VHDX
+title ExternalWin Version 1.0.12 VHDX
 call "%~dp0FileExplorerPopUp-Enable.bat" >nul 2>&1
 set /p vdisk="Input VHDX File:"
 set vdisk=%vdisk:"=%
